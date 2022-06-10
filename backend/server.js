1656fe25afb5546680e598a315815400b85aa189
Natalia Linkova <linkova1109@yandex.ru>
2022-06-10 21:47:07 +1000

    path build

--- backend/server.js
+++ backend/server.js
@@ -185,7 +185,7 @@ if (app.get("env") === "development") {
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static("/frontend/build"));
   app.get("*", (req, res) =>
     res.sendFile(
       path.resolve(__dirname, "../", "frontend", "build", "index.html")
